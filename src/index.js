a4a2bb8b1b435cef78992774b4bb6ef92bdc5f20
vunamson <vnamson231@gmail.com>
2023-06-30 00:05:24 +0700

    bai 16

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const app = express()
 const port = 3000
 app.use(express.static(path.join(__dirname,'public')))
 app.use(morgan('combined'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.engine('hbs', engine.engine({
   extname : '.hbs'
@@ -16,6 +18,17 @@ app.set('view engine', 'hbs');
 app.set('views' , path.join(__dirname,'rescor/views'))
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/serch' ,(req,res) =>{
+  res.render('serch')
+})
+
+
+
+app.post('/serch' ,(req,res) =>{
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port, () => {
